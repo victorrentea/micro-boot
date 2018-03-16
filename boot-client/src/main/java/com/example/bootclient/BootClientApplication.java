413e863ce56e67f9f5170653d6015778270b1b72
Victor Rentea <victorrentea@gmail.com>
2018-03-16 19:16:26 +0200

    circuit breaker - what does it do ??

--- boot-client/src/main/java/com/example/bootclient/BootClientApplication.java
+++ boot-client/src/main/java/com/example/bootclient/BootClientApplication.java
@@ -2,6 +2,7 @@ package com.example.bootclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,6 +12,7 @@ import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableBinding(Source.class)
 @SpringBootApplication
 @EnableEurekaClient
