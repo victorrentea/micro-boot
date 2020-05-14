012b72134831a1444a637805aae53914d26b3241
Victor Rentea <victorrentea@gmail.com>
2020-05-14 11:04:34 +0300

    rest Exchange; Doamne fereste

--- boot-client/src/main/java/com/example/bootclient/ReservationClient.java
+++ boot-client/src/main/java/com/example/bootclient/ReservationClient.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.Data;
 
 @RestController
-public class ReservationClient {
+public class ReservationClient/*<String>*/ {
     private RestTemplate restTemplate = new RestTemplate();
 //    @Value("${server.port}")
 
@@ -38,9 +39,28 @@ public class ReservationClient {
         return dishResponse.getBody().name;
     }
     @GetMapping("/reservationNames")
-    public List<String> getReservationNames() {
+    public List<String> getReservationNames(/*List<String >x*/) {
+        return restTemplate.exchange("http://localhost:8080/reservations",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Resource<List<ReservationDto>>>(){})
+            .getBody()
+            .getContent()
+            .stream()
+            .map(ReservationDto::getName)
+            .collect(toList())
+        ;
+    }
+}
 
-        return emptyList();
+class ReservationDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
