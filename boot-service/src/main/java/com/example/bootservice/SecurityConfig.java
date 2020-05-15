f98209ba7d4624aba2e89e1916816a6ea48b36c2
Victor Rentea <victorrentea@gmail.com>
2020-05-15 12:13:22 +0300

    Basic auth working

--- boot-service/src/main/java/com/example/bootservice/SecurityConfig.java
+++ boot-service/src/main/java/com/example/bootservice/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.bootservice;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
