6631120ba250072e8715ef5b8f5b990b3d3b2731
Victor Rentea <victorrentea@gmail.com>
2020-05-15 12:16:13 +0300

    acum merge login cu user:user

--- boot-service/src/main/java/com/example/bootservice/SecurityConfig.java
+++ boot-service/src/main/java/com/example/bootservice/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.example.bootservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,5 +18,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
