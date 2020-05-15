6899aad1062a0598ba8a33a23fc599ecff9c53b4
Victor Rentea <victorrentea@gmail.com>
2020-05-15 12:11:20 +0300

    Basic auth

---
+++ boot-service/src/main/java/com/example/bootservice/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.bootservice;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
--- boot-service/src/main/java/com/example/bootservice/WebServerFacotryCustomizerConfiguration.java
+++ boot-service/src/main/java/com/example/bootservice/WebServerFacotryCustomizerConfiguration.java
@@ -1,24 +1,24 @@
-package com.example.bootservice;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.util.SocketUtils;
-
-@Configuration
-public class WebServerFacotryCustomizerConfiguration implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
-
-    @Value("${port.number.min:8080}")
-    private Integer minPort;
-
-    @Value("${port.number.max:8090}")
-    private Integer maxPort;
-
-    @Override
-    public void customize(ConfigurableServletWebServerFactory factory) {
-        int port = SocketUtils.findAvailableTcpPort(minPort, maxPort);
-        factory.setPort(port);
-        System.getProperties().put("server.port", port);
-    }
-}
+//package com.example.bootservice;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.util.SocketUtils;
+//
+//@Configuration
+//public class WebServerFacotryCustomizerConfiguration implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+//
+//    @Value("${port.number.min:8080}")
+//    private Integer minPort;
+//
+//    @Value("${port.number.max:8090}")
+//    private Integer maxPort;
+//
+//    @Override
+//    public void customize(ConfigurableServletWebServerFactory factory) {
+//        int port = SocketUtils.findAvailableTcpPort(minPort, maxPort);
+//        factory.setPort(port);
+//        System.getProperties().put("server.port", port);
+//    }
+//}
